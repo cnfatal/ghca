00277fcd696767b28aee1959374a9bd080665746
zhanglikun <zhanglikun@ghostcloud.cn>
2018-09-04 17:02:16 +0800

    feature(): simplify

--- server/server.go
+++ server/server.go
@@ -1,38 +1,24 @@
 package server
 
 import (
-	"encoding/json"
 	"github.com/fatalc/ghca/encrypt"
-	"log"
 	"net/http"
 )
 
-const HELP_MSG = "电信天翼飞Young 3.09 算号工具\n使用方法: GET /?username=<username>&password=<password>"
+const HelpMsg = `电信天翼飞Young 3.09 算号工具
+使用方法: GET /?username=<username>&password=<password>
+`
 
 type GhcaHandler struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Encrypt  string `json:"encrypt,omitempty"`
-	Help     string `json:"help,omitempty"`
 }
 
-func (gh *GhcaHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func (*GhcaHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
-	gh.Username = request.URL.Query().Get("username")
-	gh.Password = request.URL.Query().Get("password")
-
-	if gh.Username == "" || gh.Password == "" {
-		writer.WriteHeader(http.StatusPaymentRequired)
-		gh.Help = HELP_MSG
+	username := request.URL.Query().Get("username")
+	password := request.URL.Query().Get("password")
+	if username == "" || password == "" {
+		writer.Write([]byte(HelpMsg))
 	} else {
-		gh.Help = ""
-		writer.WriteHeader(http.StatusOK)
-		gh.Encrypt = encrypt.GhcaEncode(gh.Username, gh.Password)
+		writer.Write([]byte(encrypt.GhcaEncode(username, password)))
 	}
-
-	result, e := json.Marshal(gh)
-	if e != nil {
-		log.Print(e.Error())
-	}
-	writer.Write(result)
 }
