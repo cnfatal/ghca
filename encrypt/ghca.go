b986ce576b638ca303c912dc985c479f069f04b0
zhanglikun <zhanglikun@ghostcloud.cn>
2018-09-05 15:27:01 +0800

    refactor(ghca): bugfixd

--- encrypt/ghca.go
+++ encrypt/ghca.go
@@ -16,48 +16,42 @@ func GhcaEncode(username, password string) string {
 	timeUnix := int(time.Now().Unix())
 	timeHex := fmt.Sprintf("%x", timeUnix)
 
-	// fixme: the results is different from the PHP's chr() function, i don't know why
-	i0 := string(timeUnix >> 24)
-	i1 := string(timeUnix >> 16)
-	i2 := string(timeUnix >> 8)
-	i3 := string(timeUnix >> 0)
-
 	accountAsciiSum := 0
 	for _, v := range username {
 		accountAsciiSum += int(v)
 	}
 	timeModPsw := timeUnix % pswLen
-	timeMod := 0
-	if timeModPsw < 1 {
-		timeMod = timeModPsw + 1
-	} else {
-		timeMod = timeModPsw
-	}
-	seed := 0
-	if timeModPsw == pswLen {
-		seed = timeMod - 1
-	} else {
-		seed = timeMod
-	}
-	accountLatFour := fmt.Sprintf("%04x", accountAsciiSum^seed)
-	splitLen := 0
-	if timeMod == pswLen {
-		splitLen = timeMod
-	} else {
-		splitLen = timeMod + 1
-	}
-	splitPswOne := string([]rune(password)[0:splitLen])
-	splitPswTwo := string([]rune(password)[splitLen:pswLen])
-	splitKeyOne := string([]rune(key)[0 : 60-len(splitPswOne)])
-	splitKeyTwo := string([]rune(key)[0 : 64-len(username)-len(splitPswTwo)])
-	md5Str := fmt.Sprintf("%s%s%s%s%s%s%s%s%s", i0, i1, i2, i3, splitKeyOne, splitPswOne, username, splitKeyTwo, splitPswTwo)
-	md5twice := hex.EncodeToString(MD5(MD5([]byte(md5Str))))
-	return "~ghca" + strings.ToUpper(timeHex+"2023"+md5twice+accountLatFour+username)
-}
+	timeMod := addOne(timeModPsw, timeModPsw < 1)
+	seed := addOne(timeMod-1, timeModPsw != pswLen)
+	splitLen := addOne(timeMod, timeMod != pswLen)
 
-func MD5(bytes []byte) []byte {
+	usernameLastFour := fmt.Sprintf("%04x", accountAsciiSum^seed)
+
+	splitPswOne := password[0:splitLen]
+	splitPswTwo := password[splitLen:]
+	splitKeyOne := key[0 : 60-splitLen]
+	splitKeyTwo := key[0 : 64-len(username)-pswLen+splitLen]
+
+	md5Str := splitKeyOne + splitPswOne + username + splitKeyTwo + splitPswTwo
+
+	bytes := make([]byte, 0)
+	bytes = append(bytes, byte(timeUnix>>24), byte(timeUnix>>16), byte(timeUnix>>8), byte(timeUnix>>0))
+	bytes = append(bytes, []byte(md5Str)...)
+
+	// md5 twice
 	hash := md5.New()
 	hash.Write(bytes)
 	sum := hash.Sum(nil)
-	return sum
+	hash.Reset()
+	hash.Write(sum)
+	md5twice := hex.EncodeToString(hash.Sum(nil))[:16]
+
+	return "~ghca" + strings.ToUpper(timeHex+"2023"+md5twice+usernameLastFour+username)
+}
+
+func addOne(in int, isAddOne bool) int {
+	if isAddOne {
+		in++
+	}
+	return in
 }
